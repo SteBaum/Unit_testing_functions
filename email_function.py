ad6e5b142b7f99bd4bfd3fc0750dc30f96629445
SteBaum <stephan.baum@edu.dsti.institute>
2022-03-03 12:27:03 +0100

    commit these first functions

---
+++ email_function.py
@@ -0,0 +1,38 @@
+import re
+
+# More elaborated version (copy to src/user_functions.py)
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Tell me your email: ")
+
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.') 
+    
+    else:
+        return email
+
+
+def get_user_name_from_input():
+    """ Not empty string. No spaces. """
+    user_name = input("Create your user name: ")
+
+    if (" " in user_name or user_name ==""):
+        print("Create your user name again: ")
+
+    else:
+        return user_name
+
+
+def get_password_from_input():
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    
+    password = input("Type your password: ")
+    numbers = re.compile('1 2 3 4 5 6 7 8 9 0')
+    special_character = re.compile('@ _ ! # $ % ^ & * ( ) < > ? / \ | { } ~ :')
+    letters = re.compile('a z e r t y u i o p q s d f g h j k l m w x c v b n A Z E R T Y U I O P Q S D F G H J K L M W X C V B N')
+    if (numbers.search(string) == None or special_character.search(string) == None or letters.search(string) == None
+    or len(password) < 8):
+        print("Password not valid")
+
+    else:
+        return "Password has successfully been updated"
---
+++ test_user_function.py
@@ -0,0 +1,25 @@
+# Tests (copy to tests/test_user_functions.py)
+import pytest
+import io
+from email_function import *
+
+def test_email_with_user_input_no_at_sign(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
+    assert get_email_from_input() is None
+
+def test_email_with_user_input_no_dot(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra@adaltascom'))
+    assert get_email_from_input() is None
+
+def test_email_with_user_input_correct(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra@adaltas.com'))
+    assert get_email_from_input() == 'petra@adaltas.com'
+
+
+def test_username_with_user_input_correct(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('wigins'))
+    assert get_user_name_from_input() == 'wigins'
+
+def test_password_with_user_input_correct(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO(''))
+    assert get_password_from_input() == 'Fake9password'
