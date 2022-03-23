a1fc77898dfc1aab61472bcf73fa787c58886ed1
SteBaum <stephan.baum@edu.dsti.institute>
2022-03-24 00:05:49 +0100

    consolidation3

--- email_function.py
+++ email_function.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 # More elaborated version (copy to src/user_functions.py)
 def get_email_from_input():
@@ -23,17 +24,20 @@ def get_user_name_from_input():
         return user_name
 
 
+
 def get_password_from_input():
     """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
     
-    message = "Password has successfully been updated"
-    special_character = ['@ ','_','!','#','$','%', '^', '&', '*', '(', ')', '<', '>', '?', '|','{','}','~',':']
     password = input("Please enter a password: ")
+    message = "Password has successfully been updated"
+
+    special_character = ['@ ','_','!','#','$','%', '^', '&', '*', '(', ')', '<', '>', '?', '|','{','}','~',':']
+    
     if len(password) < 8:
         print("Password to short, 8 characters minimum")
-    elif re.search(str([0-9]), password) is None:
-        print("At least one digit required")
-    elif re.search(str([a-z]), password) is None:
+    elif re.search(str(list(string.digits)), password) is None:
+       print("At least one digit required")
+    elif re.search(list(string.ascii_letters), password) is None:
         print("At least one letter required")
     elif re.search(str(special_character), password) is None:
             print("At least one speacial character required")
